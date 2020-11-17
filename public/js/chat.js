3842a0ab30a1c1207883f94c829ffda845aede2b
Seth McCullough <sethmckilladev@gmail.com>
2020-11-17 14:32:04 -0500

    Add profanity filter and geolocation services

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,5 +7,32 @@ socket.on("message", (message) => {
 document.querySelector("#message-form").addEventListener("submit", (event) => {
   event.preventDefault();
   const message = event.target.elements.message.value;
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message delivered!");
+  });
+});
+
+document.querySelector("#location-button").addEventListener("click", () => {
+  if (!navigator.geolocation)
+    return alert("Browser does not support geolocation services.");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      (error) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log("Location shared!");
+      }
+    );
+  });
 });
